75996fc441e3c36d5999f588d285e79307c1692f
Micah-Bello <micahbello16@gmail.com>
2021-07-13 16:12:20 +0100

    Add project files.

---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
